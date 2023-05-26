12f364c0a086f008378cf8026d66735900bb642a
Malcolm Kahora <mango22mjk@gmail.com>
2023-05-26 13:07:00 -0400

    Tcp lister with os bound port so tests work all the time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    run(TcpListener::bind("localhost:0").expect("Failed to bind port"))?.await
 }
